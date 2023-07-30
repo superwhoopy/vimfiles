e4a52793474043f89eb5b8ceda1cf7f3974d1108
Emmanuel Ohayon <eohayon@mail.ks.int>
2023-07-30 14:14:55 +0200

    move entirely to nvim, switch to lazy.nvim

--- init.vim
+++ init.vim
@@ -264,9 +264,6 @@ nmap <F10>  :StripWhitespace<CR>
 let g:strip_whitespace_on_save = 0
 let g:strip_whitespace_confirm = 0
 
-
-" Lualine ######################################################################
-
 " python-syntax ################################################################
 
 " enable Python highlighting plugin
@@ -292,7 +289,6 @@ let g:easy_align_delimiters = {
 " use 's' instead of 'S' for surrounding in visual mode (undocumented <Plug>
 " stuff: just read the source code)
 xmap   s <Plug>VSurround
-
 
 " ALE ##########################################################################
 
@@ -334,70 +330,6 @@ let g:WebDevIconsUnicodeDecorateFileNodesExtensionSymbols = {
             \ 'bgt': '祥',
             \ }
 
-" telescope ###################################################################
-
-:lua << EOF
-local fb_actions = require('telescope').extensions.file_browser.actions
-local lga_actions = require('telescope-live-grep-args.actions')
-require('telescope').setup{
-    defaults = {
-        mappings = {
-            i = {
-                ["<C-j>"] = 'move_selection_next',
-                ["<C-k>"] = 'move_selection_previous',
-                ["jj"] = { "<esc>", type="command" },
-            },
-            n = {
-                ["<C-j>"] = 'move_selection_next',
-                ["<C-k>"] = 'move_selection_previous',
-            },
-        },
-        vimgrep_arguments = {
-          "rg",
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--smart-case",
-          "--glob=!tags"
-        }
-    },
-    pickers = {
-        tags = {
-            only_sort_tags = true
-        }
-    },
-    extensions = {
-        file_browser = {
-            theme = "ivy",
-            -- disables netrw and use telescope-file-browser in its place
-            hijack_netrw = true,
-            mappings = {
-                ["i"] = {
-                    -- your custom insert mode mappings
-                    ["<Left>"] = fb_actions.goto_parent_dir
-                },
-                ["n"] = {
-                    -- your custom normal mode mappings
-                },
-            },
-            grouped = true,
-            git_status = false,
-        },
-        live_grep_args = {
-            auto_quoting = true, -- enable/disable auto-quoting
-            -- define mappings, e.g.
-            mappings = { -- extend mappings
-            i = { },
-            },
-        }
-    },
-}
-require("telescope").load_extension "file_browser"
-require("telescope").load_extension("live_grep_args")
-EOF
-
 " Startify #####################################################################
 
 let g:startify_change_to_vcs_root = 1
@@ -426,130 +358,9 @@ let g:startify_bookmarks = readfile(s:startify_bookmarks_file)
 " quickly edit the file that stores startify bookmarks
 command! StartifyEditBookmarks execute('edit ' . s:startify_bookmarks_file)
 
-" Treesitter ###################################################################
-
-:lua << EOF
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = {
-      -- "c", "rst", "bash", "json", "hjson",
-  },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "all" },
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = false,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "help", "markdown" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-EOF
-
-
 " LSP servers ##################################################################
 
 :lua << EOF
-  -- nvim-cmp for autocompletion
-  local cmp = require('cmp')
-  cmp.setup({
-      snippet = {
-          -- REQUIRED - you must specify a snippet engine
-          expand = function(args)
-                     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                   end,
-      },
-      window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-      },
-      mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          -- Accept currently selected item. Set `select` to `false` to only
-          -- confirm explicitly selected items.
-      }),
-      sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'nvim_lsp_signature_help' },
-          { name = 'vsnip' }, -- For vsnip users.
-          { name = 'buffer',
-            option = { get_bufnrs = vim.api.nvim_list_bufs },
-          },
-      })
-  })
-
-  -- Rust LS
-  require'lspconfig'.rust_analyzer.setup{}
-
-  -- Vimscript LS
-  require'lspconfig'.vimls.setup{}
-
-  -- JSON LS
-  --Enable (broadcasting) snippet capability for completion
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  require'lspconfig'.jsonls.setup {
-      capabilities = capabilities,
-  }
-
-  -- Python LS
-  capabilities = require('cmp_nvim_lsp').default_capabilities()
-  require'lspconfig'.pylsp.setup{
-    settings = {
-      pylsp = {
-        plugins = {
-          autopep8 = { enabled=false },
-          pyflakes = { enabled=false },
-
-          yapf = { enabled=true },
-          ruff = { enabled=true },
-        }
-      }
-    },
-    capabilities = capabilities,
-  }
-
-  -- Lua
-  require'lspconfig'.lua_ls.setup {
-    Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most
-          -- likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
-    },
-  }
-
   -- Global mappings.
   -- See `:help vim.diagnostic.*` for documentation on any of the below functions
   vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
@@ -589,45 +400,8 @@ EOF
   })
 EOF
 
-" Bash (requires to tweak the starting command on windows)
-execute('lua require("lspconfig").bashls.setup('
-            \ . ( has('win32')
-            \     ? '{ cmd = { "bash-language-server.cmd", "start" } }'
-            \     : '{}' )
-            \ . ')')
-
 " pretty signs
 sign define DiagnosticSignError text=🔴 texthl=DiagnosticSignError linehl= numhl=
 sign define DiagnosticSignWarn text=⚠ texthl=DiagnosticSignWarn linehl= numhl=
 sign define DiagnosticSignInfo text=ℹ texthl=DiagnosticSignInfo linehl= numhl=
 sign define DiagnosticSignHint text=👉 texthl=DiagnosticSignHint linehl= numhl=
-
-
-" leap #########################################################################
-
-lua require('leap').set_default_keymaps()
-
-" NoNeckPain ###################################################################
-
-:lua << EOF
-require("no-neck-pain").setup({
-      mappings = {
-          enabled = true,
-          toggle = "<Leader>np",
-          widthUp = false,
-          widthDown = false,
-          scratchPad = false,
-      },
-
-      buffers = {
-          setNames = false,
-          -- colors to apply to both side buffers, for buffer scopped options @see |NoNeckPain.bufferOptions|
-          --- see |NoNeckPain.bufferOptionsColors|
-          colors = {
-              background = nil,
-              blend = 0.1,
-              text = nil,
-          },
-      },
-})
-EOF
--- lua/packages.lua
+++ lua/packages.lua
@@ -1,11 +1,12 @@
 local P = {}
 packages = P
 
--- ###########################################################
+-- #############################################################################
 -- PLUGINS CONFIGURATION
--- ###########################################################
+-- #############################################################################
 
--- LUALINE ------------------------------------------------------
+-- LUALINE ---------------------------------------------------------------------
+
 local lualine_opts = {
   options = {
     icons_enabled = true,
@@ -50,91 +51,314 @@ local lualine_opts = {
   extensions = {}
 }
 
--- ###########################################################
+-- TELESCOPE -------------------------------------------------------------------
+
+local function telescope_fn()
+  local fb_actions = require('telescope').extensions.file_browser.actions
+  require('telescope').setup{
+      defaults = {
+          mappings = {
+              i = {
+                  ["<C-j>"] = 'move_selection_next',
+                  ["<C-k>"] = 'move_selection_previous',
+                  ["jj"] = { "<esc>", type="command" },
+              },
+              n = {
+                  ["<C-j>"] = 'move_selection_next',
+                  ["<C-k>"] = 'move_selection_previous',
+              },
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--glob=!tags"
+          }
+      },
+      pickers = {
+          tags = {
+              only_sort_tags = true
+          }
+      },
+      extensions = {
+          file_browser = {
+              theme = "ivy",
+              -- disables netrw and use telescope-file-browser in its place
+              hijack_netrw = true,
+              mappings = {
+                  ["i"] = {
+                      -- your custom insert mode mappings
+                      ["<Left>"] = fb_actions.goto_parent_dir
+                  },
+                  ["n"] = {
+                      -- your custom normal mode mappings
+                  },
+              },
+              grouped = true,
+              git_status = false,
+          },
+          live_grep_args = {
+              auto_quoting = true, -- enable/disable auto-quoting
+              -- define mappings, e.g.
+              mappings = { -- extend mappings
+              i = { },
+              },
+          }
+      },
+  }
+  require("telescope").load_extension "file_browser"
+  require("telescope").load_extension("live_grep_args")
+end
+
+
+-- TREESITTER ------------------------------------------------------------------
+
+local function nvim_treesitter_fn()
+  require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = {
+      -- "c", "rst", "bash", "json", "hjson",
+    },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = { "all" },
+
+    highlight = {
+      -- `false` will disable the whole extension
+      enable = false,
+
+      -- NOTE: these are the names of the parsers and not the filetype. (for
+      -- example if you want to disable highlighting for the `tex` filetype, you
+      -- need to include `latex` in this list as this is the name of the parser)
+      -- list of language that will be disabled
+      disable = { "help", "markdown" },
+
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same
+      -- time. Set this to `true` if you depend on 'syntax' being enabled (like
+      -- for indentation). Using this option may slow down your editor, and you
+      -- may see some duplicate highlights. Instead of true it can also be a
+      -- list of languages
+      additional_vim_regex_highlighting = false,
+    },
+  }
+end
+
+
+-- NO NECK PAIN ----------------------------------------------------------------
+local noneckpain_opts = {
+  mappings = {
+    enabled = true,
+    toggle = "<Leader>np",
+    widthUp = false,
+    widthDown = false,
+    scratchPad = false,
+  },
+
+  buffers = {
+    setNames = false,
+    -- colors to apply to both side buffers, for buffer scopped options @see
+    -- |NoNeckPain.bufferOptions|
+    --- see |NoNeckPain.bufferOptionsColors|
+    colors = {
+      background = nil,
+      blend = 0.1,
+      text = nil,
+    },
+  },
+}
+
+-- LSP SERVERS -----------------------------------------------------------------
+
+-- nvim-cmp (autocompletion)
+local function nvim_cmp_fn()
+  local cmp = require('cmp')
+  cmp.setup({
+      snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+                     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                   end,
+      },
+      window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          -- Accept currently selected item. Set `select` to `false` to only
+          -- confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lsp_signature_help' },
+          { name = 'vsnip' }, -- For vsnip users.
+          { name = 'buffer',
+            option = { get_bufnrs = vim.api.nvim_list_bufs },
+          },
+      })
+  })
+end
+
+-- lspconfig for various languages
+local function lspconfig_fn()
+  -- Rust LS
+  require'lspconfig'.rust_analyzer.setup{}
+
+  -- Vimscript LS
+  require'lspconfig'.vimls.setup{}
+
+  -- JSON LS
+  --Enable (broadcasting) snippet capability for completion
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  require'lspconfig'.jsonls.setup {
+      capabilities = capabilities,
+  }
+
+  -- Python LS
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
+  require'lspconfig'.pylsp.setup{
+    settings = {
+      pylsp = {
+        plugins = {
+          autopep8 = { enabled=false },
+          pyflakes = { enabled=false },
+
+          yapf = { enabled=true },
+          ruff = { enabled=true },
+        }
+      }
+    },
+    capabilities = capabilities,
+  }
+
+  -- Lua
+  require'lspconfig'.lua_ls.setup {
+    Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most
+          -- likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+    },
+  }
+
+  -- Bash
+  local _bash_opts = {}
+  if vim.fn.has('win32') then
+    _bash_opts = { cmd = { "bash-language-server.cmd", "start" } }
+  end
+  require('lspconfig').bashls.setup(_bash_opts)
+end
+
+-- #############################################################################
 -- PLUGINS LISTING
--- ###########################################################
+-- #############################################################################
 
 P.plugins = {
-    'aklt/plantuml-syntax',
-        -- syntax highlighting
-    'cespare/vim-toml',
-        -- syntax highlighting
-    'dhruvasagar/vim-table-mode',
-    'dstein64/vim-startuptime',
-        -- profiling: run nvim --startuptime
-    'editorconfig/editorconfig-vim',
-    {'folke/tokyonight.nvim', branch='main'},
-        -- colorscheme
+  'aklt/plantuml-syntax',
+  'cespare/vim-toml',
+  'dhruvasagar/vim-table-mode',
+
+  'dstein64/vim-startuptime', -- profiling: run nvim --startuptime
+
+  'editorconfig/editorconfig-vim',
+
+  {'folke/tokyonight.nvim', branch='main'}, -- colorscheme
+
+  {
     'ggandor/leap.nvim',
-        -- easymotion-like (s)
-    'hiphish/jinja.vim',
-        -- syntax highlighting
+    init=function()
+      require('leap').set_default_keymaps()
+    end
+  }, -- easymotion-like (s)
 
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
+  'hiphish/jinja.vim',
 
-    'itchyny/vim-cursorword',
-        -- underline the word under the cursor
-    'jeffkreeftmeijer/vim-numbertoggle',
-        -- disable relative line number on disabled buffers
-    'jenterkin/vim-autosource',
-    'jremmen/vim-ripgrep',
-    'junegunn/vim-easy-align',
-    'justinmk/vim-syntax-extra',
-        -- extraneous enhanced(?) syntax highlighting, for C, flex, bison
-    'krono-safe/vim-asterios',
-    'lepture/vim-velocity',
-        -- syntax highlighting
-    'LnL7/vim-nix',
-    'Lokaltog/vim-easymotion',
-    'MarcWeber/vim-addon-mw-utils',
-    'martinda/Jenkinsfile-vim-syntax',
-        -- syntax highlighting
-    'majutsushi/tagbar',
-    'mhinz/vim-startify',
-    'morhetz/gruvbox',
-        -- colorscheme
-    'ngg/vim-gn',
-        -- syntax highlighting
-    'ntpeters/vim-better-whitespace',
-    'neovim/nvim-lspconfig',
-    'neovimhaskell/haskell-vim',
-    'nvim-lua/plenary.nvim',
+  {'hrsh7th/nvim-cmp', config=nvim_cmp_fn},
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-nvim-lsp-signature-help',
+  'hrsh7th/cmp-vsnip',
+  'hrsh7th/vim-vsnip',
 
-    {'nvim-lualine/lualine.nvim', opts=lualine_opts},
+  'itchyny/vim-cursorword', -- underline the word under the cursor
+  'jeffkreeftmeijer/vim-numbertoggle', -- disable relative line number on
+                                       -- disabled buffers
+  'jenterkin/vim-autosource',
+  'jremmen/vim-ripgrep',
+  'junegunn/vim-easy-align',
+  'justinmk/vim-syntax-extra', -- extraneous enhanced(?) syntax highlighting,
+                               -- for C, flex, bison
+  'krono-safe/vim-asterios',
+  'lepture/vim-velocity',
+  'LnL7/vim-nix', -- syntax highlighting
+  'Lokaltog/vim-easymotion',
+  'MarcWeber/vim-addon-mw-utils',
+  'martinda/Jenkinsfile-vim-syntax', -- syntax highlighting
+  'majutsushi/tagbar',
+  'mhinz/vim-startify',
+  'morhetz/gruvbox', -- colorscheme
+  'ngg/vim-gn', -- syntax highlighting
+  'ntpeters/vim-better-whitespace',
+  {'neovim/nvim-lspconfig', config=lspconfig_fn},
+  'neovimhaskell/haskell-vim',
+  'nvim-lua/plenary.nvim',
 
-    'nvim-telescope/telescope.nvim',
-    'nvim-telescope/telescope-symbols.nvim',
-    'nvim-telescope/telescope-file-browser.nvim',
-    'nvim-telescope/telescope-live-grep-args.nvim',
-    'nvim-tree/nvim-web-devicons',
-    {'nvim-treesitter/nvim-treesitter', build=':TSUpdate'},
-    'raimon49/requirements.txt.vim',
-        -- syntax highlighting
-    'sainnhe/everforest',
-        -- colorscheme
-    'shortcuts/no-neck-pain.nvim',
-    'shime/vim-livedown',
-    'tmhedberg/SimpylFold', -- better Python folding
-    'tomtom/tlib_vim',
-    'tpope/vim-dispatch',
-        -- use :Make instead of :make to run in background
-    'tpope/vim-commentary',
-    'tpope/vim-fugitive',
-    'tpope/vim-surround',
-    'vim-latex/vim-latex',
-    'vim-python/python-syntax',
-    'vim-scripts/DrawIt',
-    'Vimjas/vim-python-pep8-indent',
-    'w0rp/ale',
-    'zchee/vim-flatbuffers',
-        -- syntax highlighting
+  {'nvim-lualine/lualine.nvim', opts=lualine_opts},
 
-    { url='ssh://git@dev.ks.int:7999/prot/vim-ks.git' },
+  {'nvim-telescope/telescope.nvim', config=telescope_fn},
+  'nvim-telescope/telescope-symbols.nvim',
+  'nvim-telescope/telescope-file-browser.nvim',
+  'nvim-telescope/telescope-live-grep-args.nvim',
+  'nvim-tree/nvim-web-devicons',
+
+  {'nvim-treesitter/nvim-treesitter',
+  build=':TSUpdate',
+  config=nvim_treesitter_fn},
+
+  'raimon49/requirements.txt.vim', -- syntax highlighting
+  'sainnhe/everforest', -- colorscheme
+
+  {'shortcuts/no-neck-pain.nvim', opts=noneckpain_opts},
+  'shime/vim-livedown',
+  'tmhedberg/SimpylFold', -- better Python folding
+  'tomtom/tlib_vim',
+  'tpope/vim-dispatch', -- use :Make instead of :make to run in background
+  'tpope/vim-commentary',
+  'tpope/vim-fugitive',
+  'tpope/vim-surround',
+  'vim-latex/vim-latex',
+  'vim-python/python-syntax',
+  'vim-scripts/DrawIt',
+  'Vimjas/vim-python-pep8-indent',
+  'w0rp/ale',
+  'zchee/vim-flatbuffers', -- syntax highlighting
+
+  { url='ssh://git@dev.ks.int:7999/prot/vim-ks.git' },
 }
 
 if vim.fn.has('win32') then

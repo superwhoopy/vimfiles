dc304ee7e6ff307cfd9c92423d998ae2cf75592c
Emmanuel Ohayon <eohayon@mail.ks.int>
2023-09-15 00:42:25 +0200

    fix something

--- lua/packages.lua
+++ lua/packages.lua
@@ -313,6 +313,7 @@ local function lspconfig_fn()
         workspace = {
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = {
